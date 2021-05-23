0d39f5f5d5416c51dfac806454f29c214fe390e6
ayansome1 <ayansome1@gmail.com>
2021-05-23 20:40:37 +0530

    init

---
+++ src/contracts/Color.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Color is ERC721 {
+  string[] public colors;
+  mapping(string => bool) _colorExists;
+
+  constructor() ERC721("Color", "COLOR") {
+  }
+
+  // E.G. color = "#FFFFFF"
+  function mint(string memory _color) public {
+    require(!_colorExists[_color]);
+    colors.push(_color);
+    uint _id = colors.length;
+    _mint(msg.sender, _id);
+    _colorExists[_color] = true;
+  }
+
+}
